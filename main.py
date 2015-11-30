45f858f3c1b5727712072f6dc76590f5e028c8e0
System Administrator <root@Arslans-MacBook-Pro.local>
2015-11-30 10:37:34 +0000

    additions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 ## Total process for finding TWs/RPOs                                        ##
 ## ========================================================================= ##
 ## The code presented provides a complete method of finding edge states      ##
-## using velocity fields generated using the rsolvent formulation as         ##
+## using velocity fields generated using the rsolvent formulation as         ##
 ## an initial condition.                                                     ##
 ##                                                                           ##
 ## The program works by simply providing a configuration file (there is a    ##
@@ -20,13 +20,24 @@
 ##                                                                           ##
 ###############################################################################
 
+import sys
+import os
+import argparse
+pwd = os.getcwd()
+
+sys.path.insert(0, pwd+'/modes')
+sys.path.insert(1, pwd+'/dns')
+sys.path.insert(2, pwd+'/post_process')
+sys.path.insert(3, pwd+'/nkh')
+sys.path.insert(4, pwd+'/misc')
+
 import utils as ut
 import generate_modes as gm
-import dns as d
+import dns
 import postpro_standard as ps
-import nkh as n
-import argparse
-import sys
+#import nkh
+from datetime import datetime
+import time
 
 parser = argparse.ArgumentParser(description='Total process to find edge states. \nPlease provide a directory to carry out the process in.')
 parser.add_argument('-d', help='directory to output process in.', action='store')
@@ -36,16 +47,23 @@ ut.printEdgeStatesStart()
 print('\nThe process will be carried out in:')
 print(args.directory, '\n')
 
+
+startTime_1 = datetime.now()
 print('=================================================')
 print('\n#1 Generating the initial condition (using resolvent formulation)\n')
 gm.generate_initial_condition(args.directory)
+endTime_1 = datetime.now() - startTime_1
 
+startTime_2 = datetime.now()
 print('=================================================')
 print('\n#2 Running DNS\n')
-d.run_dns(args.directory)
+dns.run(args.directory)
+endTime_2 = datetime.now() - startTime_2
 
+startTime_3 = datetime.now()
 print('=================================================')
 print('\n#3 Post-processing the DNS simulation\n')
 ps.run_postpro_standard(args.directory)
+endTime_3 = datetime.now() - startTime_3
 
 print('\nFinished!\n')
---
+++ modes/generate_modes.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import utils as ut
+import time
+
+
+
+# if configuration file exists:
+#   Read configuration file.
+# else:
+#   Exit error.
+
+# Based on configuration file, we go through the whole resolvent process.
+
